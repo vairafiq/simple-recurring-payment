499a106414844f42e1718d2da59a163a35cf68c2
vairafiq <vairafiq@gmail.com>
2022-08-16 15:42:00 +0600

    add confirmation block

---
+++ src/payment-confirmation/edit.js
@@ -0,0 +1,91 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( { attributes, setAttributes } ) {
+	return (
+		<div { ...useBlockProps() } >
+			<div className='srp_admin_container'>
+			<div>
+				<h4>{ __( 'Payment Form', 'srp' ) }</h4>
+			</div>
+			<div className='srp_admin_form'>
+				
+				<p>
+					<label for='srp_title'>{ __( 'Title', 'srp' ) }</label>
+					<input id='srp_title' value={ attributes.title } onChange={ ( event ) => { setAttributes( { title: event.target.value } ) } } type='text' className='srp_title'/>
+				</p>
+				<p>
+					<label for='srp_form_subtitle'>{ __( 'Form Subtitle', 'srp' ) }</label>
+					<input id='srp_form_subtitle' value={ attributes.form_subtitle } onChange={ ( event ) => { setAttributes( { form_subtitle: event.target.value } ) } } type='text' placeholder={ __( 'Start collecting money right way!', 'srp' ) } className='form_subtitle'/>
+				</p>
+				<p>
+					<label for='srp_price'>{ __( 'Default Price', 'srp' ) }</label>
+					<input id='srp_price' value={ attributes.price } onChange={ ( event ) => { setAttributes( { price: event.target.value } ) } } type='number' className='srp_price'/>
+				</p>
+				<p>
+					<label for='srp_vat'>{ __( 'Vat (%)', 'srp' ) }</label>
+					<input id='srp_vat' value={ attributes.vat } onChange={ ( event ) => { setAttributes( { vat: event.target.value } ) } } type='number' className='srp_vat'/>
+				</p>
+				<p>
+					<label for='srp_recurring'>{ __( 'Allow Recurring', 'srp' ) }</label>
+					<input id='srp_recurring' checked={attributes.recurring} onChange={ ( event ) => { 
+						const target = event.target;
+						let is_recurring = target.type === 'checkbox' ? target.checked : target.value;
+						setAttributes( { 
+							recurring: is_recurring
+						} );
+						} } type='checkbox' className='srp_recurring'/>
+				</p>
+				{ attributes.recurring === true && (
+				<p>
+					<label for='srp_recurring_term'>{ __( 'Default Recurring Term', 'srp' ) }</label>
+					<select id='srp_recurring_term' value={ attributes.recurring_term } onChange={ ( event ) => { setAttributes( { recurring_term: event.target.value } ) } } className='srp_recurring_term'>
+						<option value='yearly'>{ __( 'Yearly', 'srp' ) }</option>
+						<option value='monthly'>{ __( 'Monthly', 'srp' ) }</option>
+						<option value='weekly'>{ __( 'Weekly', 'srp' ) }</option>
+						<option value='daily'>{ __( 'Daily', 'srp' ) }</option>
+					</select>
+				</p>
+				)}
+				<p>
+					<label for='srp_gateway'>{ __( 'Default Gateway', 'srp' ) }</label>
+					<select id='srp_gateway' value={ attributes.gateway } onChange={ ( event ) => { setAttributes( { gateway: event.target.value } ) } } className='srp_gateway'>
+						<option value='stripe'>{ __( 'Stripe', 'srp' ) }</option>
+						<option value='paypal'>{ __( 'PayPal', 'srp' ) }</option>
+						<option value='authorize'>{ __( 'Authorize.net', 'srp' ) }</option>
+						<option value='2checkout'>{ __( '2 Checkout', 'srp' ) }</option>
+					</select>
+				</p>
+			</div>
+			</div>
+			</div>
+	);
+}
---
+++ src/payment-confirmation/save.js
@@ -0,0 +1,64 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ */
+ import { __ } from '@wordpress/i18n';
+
+ /**
+  * React hook that is used to mark the block wrapper element.
+  * It provides all the necessary props like the class name.
+  *
+  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
+  */
+ 
+ /**
+  * The save function defines the way in which the different attributes should
+  * be combined into the final markup, which is then serialized by the block
+  * editor into `post_content`.
+  *
+  * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#save
+  *
+  * @return {WPElement} Element to render.
+  */
+ export default function save ( args )
+ {
+	 return (
+		 <div>
+			 <h1 className='srpHeader'>{ args.attributes.title }</h1>
+			 <div className='srpDetails'>
+				 { args.attributes.price && <p className='srpDetails'> { args.attributes.price } </p> }
+				 { !args.attributes.price && <div>
+					<label for='srp_price'>{ __( 'Price', 'srp' ) }</label>
+					<input id='srp_price' placeholder = 'Enter amount' type='number' className='srp_price'/>
+				 </div>
+				 }
+				 {args.attributes.recurring &&
+					 <p className='srpDetails'> { args.attributes.recurring } </p>
+				 }
+				 { args.attributes.recurring_term &&
+					 <p className='srpDetails'> { args.attributes.recurring_term } </p>
+				 }
+				 <p className='srpDetails'> { args.attributes.gateway } </p>
+				 { args.attributes.vat &&
+					 <p className='srpDetails'> { args.attributes.vat } </p>
+				 }
+			 </div>
+			 <form className='srpHeader' method="POST">
+				 <input type="number" placeholder="Enter Amount to Donate"></input>
+				 { args.attributes.recurring &&
+					 <><input type="radio" name="srpRecurring" value="yes">Yes</input><input type="radio" name="srpRecurring" value="no">No</input></>
+				 }
+				 { args.attributes.recurring_term &&
+					 <select id="srpRecurringTerm">
+						 <option className="daily">Daily</option>
+						 <option className="weekly">Weekly</option>
+						 <option className="monthly">Monthly</option>
+						 <option className="yearly">Yearly</option>
+					 </select>
+				 }
+				 <button type="submit"> Donate Now </button>
+			 </form>
+		 </div>
+	 );
+ }
